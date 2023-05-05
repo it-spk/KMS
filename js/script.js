41f9bd5a97810c613b081a112845c594ac9bde2a
Alisher Turubekov <develepor@mail.ru>
2023-05-05 12:06:25 +0600

    1.0.0

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function myFunction(){
+    let copyText = document.getElementById("myInput");
+    copyText.select();
+    document.execCommand("copy");
+};
+function myFunction2(){
+    let copyText = document.getElementById("myInput2");
+    copyText.select();
+    document.execCommand("copy");
+};
+function myFunction3(){
+    let copyText = document.getElementById("myInput3");
+    copyText.select();
+    document.execCommand("copy");
+};
+
